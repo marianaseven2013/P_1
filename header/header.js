d99c2c07ff2bf5084b945cee192ecf83dfe92c5a
marianaseven2013 <gmluch@scl.edu.gt>
2024-07-08 18:33:12 -0600

    Creacion de header

--- header/header.js
+++ header/header.js
@@ -2,6 +2,14 @@ let fouu = document.querySelector(".header");
 
 fouu.innerHTML = `
 
-        <div class="adre">Hola</div>
+        <div class="adre">
+                <h1>  WELCOME!!! believe </h1>
+                <div class="si">
+                        <a href="https://github.com/marianaseven2013/P_1">Git hub</a>
+                </div>
+                <div class="sisimg">
+                       <img src="https://i.pinimg.com/564x/8d/5c/dc/8d5cdc8556d486803a7016a0cc5c349c.jpg" alt="">
+                </div>
+        </div>
 
 `;
